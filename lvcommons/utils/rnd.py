552ad6500db8d9cadf9a47a4fdff93d4b8254f7c
lvercelli <lvercellih@gmail.com>
2015-02-26 10:43:15 -0500

    Correcciones de typos

--- lvcommons/utils/rnd.py
+++ lvcommons/utils/rnd.py
@@ -2,10 +2,10 @@ import string
 import random
 
 
-ESPECIAL_CHARS = ',;.:-_!@#$%&/=?'
+SPECIAL_CHARS = ',;.:-_!@#$%&/=?'
 
 
-def _get_src_str_for_rnd(digits=True, upper_case=True, lower_case=True, especials=False, extra=None):
+def _get_src_str_for_rnd(digits=True, upper_case=True, lower_case=True, specials=False, extra=None):
     src = []
     if digits:
         src += string.digits
@@ -13,36 +13,36 @@ def _get_src_str_for_rnd(digits=True, upper_case=True, lower_case=True, especial
         src += string.ascii_uppercase
     if lower_case:
         src += string.ascii_lowercase
-    if especials:
-        src += ESPECIAL_CHARS
+    if specials:
+        src += SPECIAL_CHARS
     if extra:
         src += extra
     return src
 
 
-def generate_random_string(length, digits=True, upper_case=True, lower_case=True, especials=False, extra=None):
+def generate_random_string(length, digits=True, upper_case=True, lower_case=True, specials=False, extra=None):
     """
     Genera una cadena de texto aleatoria, útil para códigos de confirmación
     :param length: Longitud que la cadena debe tener
     :return:
     """
-    src = _get_src_str_for_rnd(digits, upper_case, lower_case, especials, extra)
+    src = _get_src_str_for_rnd(digits, upper_case, lower_case, specials, extra)
     sr = random.SystemRandom()
     return ''.join(sr.choice(src) for x in range(length))
 
 
-def get_random_string_generator(length, digits=True, upper_case=True, lower_case=True, especials=False, extra=None):
+def get_random_string_generator(length, digits=True, upper_case=True, lower_case=True, specials=False, extra=None):
     """
     Crea un generador de cadenas aleatorias en base a los parámetros dados
     :param length:
     :param digits:
     :param upper_case:
     :param lower_case:
-    :param especials:
+    :param specials:
     :param extra:
     :return:
     """
-    src = _get_src_str_for_rnd(digits, upper_case, lower_case, especials, extra)
+    src = _get_src_str_for_rnd(digits, upper_case, lower_case, specials, extra)
     sr = random.SystemRandom()
     while True:
         yield ''.join(sr.choice(src) for x in range(length))
